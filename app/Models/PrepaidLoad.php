ee44bba8b646094aec12433f0a72a01ad0221a2f
elmer <appearonclouds@gmail.com>
2021-03-25 09:38:30 +0800

    add config details to model

--- app/Models/PrepaidLoad.php
+++ app/Models/PrepaidLoad.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class PrepaidLoad extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'prepaid_loads';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'old_prepaid_load_id',
+        'prepaid_type',
+        'reward_keyword',
+        'amax_keyword',
+        'amount',
+        'status',
+    ];
 }
