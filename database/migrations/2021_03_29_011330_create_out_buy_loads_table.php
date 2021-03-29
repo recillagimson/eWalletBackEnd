458ae941fc79fbd340ff77c4d143974099fc3291
elmer <appearonclouds@gmail.com>
2021-03-29 09:27:25 +0800

    migration and model for out buy loads

---
+++ database/migrations/2021_03_29_011330_create_out_buy_loads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOutBuyLoadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('out_buy_loads', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreign('user_account_id')->references('id')->on('users');
+            $table->foreign('prepaid_load_id')->references('id')->on('prepaid_loads');
+            $table->decimal('total_amount', 10, 3);
+            $table->timestamp('transaction_date')->nullable();
+            // $table->foreign('transaction_category_id')->references('id')->on('transaction_categories');
+            $table->string('transaction_remarks');
+            $table->uuid('user_created')->nullable();
+            $table->uuid('user_updated')->nullable();
+            $table->timestamps();
+            $table->timestamps();
+            $table->timestamp('expires_at')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('out_buy_loads');
+    }
+}
