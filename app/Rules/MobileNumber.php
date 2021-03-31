76906f57fd8689539e4161aca9476746c9ed3df7
Julius Dallego <julius.dallego@hotmail.com>
2021-04-01 06:44:29 +0800

    SVC-41 - added password format configurations - applied password format configurations in register and reset password endpoints - added validation rule for mobile number (63 + 10 digits mobile number) - applied mobile number validation rule in register, reset password & login

---
+++ app/Rules/MobileNumber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class MobileNumber implements Rule
+{
+    /**
+     * Determines if the field is a valid philippine phone
+     * number
+     *
+     * @var bool
+     */
+    private bool $validPhoneNumber = true;
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        $this->validPhoneNumber =  ((bool) preg_match('/^(639)\d{9}$/', $value));
+        if(!$this->validPhoneNumber) return false;
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        if(!$this->validPhoneNumber)
+            return 'The mobile number is invalid. Use the format 63 + 10 digit mobile number.';
+
+        return '';
+    }
+}
