5f61697f7f275276ca09acf5fd6b1f67af48f871
elmer <appearonclouds@gmail.com>
2021-04-12 15:08:52 +0800

    nature of work crud

--- app/Http/Controllers/UserUtilities/NatureOfWorkController.php
+++ app/Http/Controllers/UserUtilities/NatureOfWorkController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\UserUtilities;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\UserUtilities\NatureOfWork\INatureOfWorkRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use App\Services\Encryption\IEncryptionService;
+use App\Http\Requests\UserUtilities\NatureOfWorkRequest;
+use App\Models\UserUtilities\NatureOfWork;
 
 class NatureOfWorkController extends Controller
 {
 
     private IEncryptionService $encryptionService;
-    private IMaritalStatusRepository $maritalStatusRepository;
+    private INatureOfWorkRepository $natureOfWorkRepository;
     
-    public function __construct(IMaritalStatusRepository $maritalStatusRepository,
+    public function __construct(INatureOfWorkRepository $natureOfWorkRepository,
                                 IEncryptionService $encryptionService)
     {
-        $this->maritalStatusRepository = $maritalStatusRepository;
+        $this->natureOfWorkRepository = $natureOfWorkRepository;
         $this->encryptionService = $encryptionService;
     }
 
@@ -25,7 +31,7 @@ class NatureOfWorkController extends Controller
      */
     public function index(): JsonResponse
     {
-        $records = $this->maritalStatusRepository->getAll();
+        $records = $this->natureOfWorkRepository->getAll();
 
         $encryptedResponse = $this->encryptionService->encrypt($records->toArray());
         return response()->json($encryptedResponse, Response::HTTP_OK);
@@ -34,35 +40,46 @@ class NatureOfWorkController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request $request
+     * @param  NatureOfWorkRequest $request
      * @return JsonResponse
      */
-    public function store(Request $request): JsonResponse
+    public function store(NatureOfWorkRequest $request): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $createRecord = $this->natureOfWorkRepository->create($inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt($createRecord->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  NatureOfWork $nature_of_work
      * @return JsonResponse
      */
-    public function show($id): JsonResponse
+    public function show(NatureOfWork $nature_of_work): JsonResponse
     {
-        //
+        $encryptedResponse = $this->encryptionService->encrypt($nature_of_work->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  NatureOfWorkRequest $request
+     * @param  NatureOfWork $nature_of_work
      * @return JsonResponse
      */
-    public function update(Request $request, $id): JsonResponse
+    public function update(NatureOfWorkRequest $request, NatureOfWork $nature_of_work): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $updateRecord = $this->natureOfWorkRepository->update($nature_of_work, $inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt(array($updateRecord));
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
@@ -71,8 +88,19 @@ class NatureOfWorkController extends Controller
      * @param  int  $id
      * @return JsonResponse
      */
-    public function destroy($id): JsonResponse
+    public function destroy(NatureOfWork $nature_of_work): JsonResponse
     {
-        //
+        $deleteRecord = $this->natureOfWorkRepository->delete($nature_of_work);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    private function inputBody(array $details, string $user_id): array {
+        $body = array(
+                    'description'=>$details['description'],
+                    'status'=>$details['status'],
+                    'user_created'=>$user_id,
+                );
+        return $body;
     }
 }
