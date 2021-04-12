a6e2366ee123f4f27169faaec5d939cdfd5d7eda
elmer <appearonclouds@gmail.com>
2021-04-12 15:36:23 +0800

    signup host crud

--- app/Http/Controllers/UserUtilities/SignupHostController.php
+++ app/Http/Controllers/UserUtilities/SignupHostController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\UserUtilities;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\UserUtilities\SignupHost\ISignupHostRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use App\Services\Encryption\IEncryptionService;
+use App\Http\Requests\UserUtilities\SignupHostRequest;
+use App\Models\UserUtilities\SignupHost;
 
 class SignupHostController extends Controller
 {
 
     private IEncryptionService $encryptionService;
-    private IMaritalStatusRepository $maritalStatusRepository;
+    private ISignupHostRepository $signupHostRepository;
     
-    public function __construct(IMaritalStatusRepository $maritalStatusRepository,
+    public function __construct(ISignupHostRepository $signupHostRepository,
                                 IEncryptionService $encryptionService)
     {
-        $this->maritalStatusRepository = $maritalStatusRepository;
+        $this->signupHostRepository = $signupHostRepository;
         $this->encryptionService = $encryptionService;
     }
 
@@ -25,7 +31,7 @@ class SignupHostController extends Controller
      */
     public function index(): JsonResponse
     {
-        $records = $this->maritalStatusRepository->getAll();
+        $records = $this->signupHostRepository->getAll();
 
         $encryptedResponse = $this->encryptionService->encrypt($records->toArray());
         return response()->json($encryptedResponse, Response::HTTP_OK);
@@ -34,45 +40,67 @@ class SignupHostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request $request
+     * @param  SignupHostRequest $request
      * @return JsonResponse
      */
-    public function store(Request $request): JsonResponse
+    public function store(SignupHostRequest $request): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $createRecord = $this->signupHostRepository->create($inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt($createRecord->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  SignupHost $signup_host
      * @return JsonResponse
      */
-    public function show($id): JsonResponse
+    public function show(SignupHost $signup_host): JsonResponse
     {
-        //
+        $encryptedResponse = $this->encryptionService->encrypt($signup_host->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  SignupHostRequest $request
+     * @param  SignupHost $signup_host
      * @return JsonResponse
      */
-    public function update(Request $request, $id): JsonResponse
+    public function update(SignupHostRequest $request, SignupHost $signup_host): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $updateRecord = $this->signupHostRepository->update($signup_host, $inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt(array($updateRecord));
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  SignupHost $signup_host
      * @return JsonResponse
      */
-    public function destroy($id): JsonResponse
+    public function destroy(SignupHost $signup_host): JsonResponse
     {
-        //
+        $deleteRecord = $this->signupHostRepository->delete($signup_host);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    private function inputBody(array $details, string $user_id): array {
+        $body = array(
+                    'description'=>$details['description'],
+                    'status'=>$details['status'],
+                    'user_created'=>$user_id,
+                );
+        return $body;
     }
 }
