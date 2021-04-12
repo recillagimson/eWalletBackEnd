865496a151fc81f0ffaa7e8055bc0c07e1c40021
elmer <appearonclouds@gmail.com>
2021-04-12 15:26:29 +0800

    currency crud

--- app/Http/Controllers/UserUtilities/CurrencyController.php
+++ app/Http/Controllers/UserUtilities/CurrencyController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\UserUtilities;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\UserUtilities\Currency\ICurrencyRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use App\Services\Encryption\IEncryptionService;
+use App\Http\Requests\UserUtilities\CurrencyRequest;
+use App\Models\UserUtilities\Currency;
 
 class CurrencyController extends Controller
 {
 
     private IEncryptionService $encryptionService;
-    private IMaritalStatusRepository $maritalStatusRepository;
+    private ICurrencyRepository $currencyRepository;
     
-    public function __construct(IMaritalStatusRepository $maritalStatusRepository,
+    public function __construct(ICurrencyRepository $currencyRepository,
                                 IEncryptionService $encryptionService)
     {
-        $this->maritalStatusRepository = $maritalStatusRepository;
+        $this->currencyRepository = $currencyRepository;
         $this->encryptionService = $encryptionService;
     }
 
@@ -25,7 +31,7 @@ class CurrencyController extends Controller
      */
     public function index(): JsonResponse
     {
-        $records = $this->maritalStatusRepository->getAll();
+        $records = $this->currencyRepository->getAll();
 
         $encryptedResponse = $this->encryptionService->encrypt($records->toArray());
         return response()->json($encryptedResponse, Response::HTTP_OK);
@@ -34,45 +40,68 @@ class CurrencyController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request $request
+     * @param  CurrencyRequest $request
      * @return JsonResponse
      */
-    public function store(Request $request): JsonResponse
+    public function store(CurrencyRequest $request): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $createRecord = $this->currencyRepository->create($inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt($createRecord->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Currency $currency
      * @return JsonResponse
      */
-    public function show($id): JsonResponse
+    public function show(Currency $currency): JsonResponse
     {
-        //
+        $encryptedResponse = $this->encryptionService->encrypt($currency->toArray());
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  CurrencyRequest $request
+     * @param  Currency $currency
      * @return JsonResponse
      */
-    public function update(Request $request, $id): JsonResponse
+    public function update(CurrencyRequest $request, Currency $currency): JsonResponse
     {
-        //
+        $details = $request->validated();
+        $inputBody = $this->inputBody($details, $request->user()->id);
+        $updateRecord = $this->currencyRepository->update($currency, $inputBody);
+
+        $encryptedResponse = $this->encryptionService->encrypt(array($updateRecord));
+        return response()->json($encryptedResponse, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Currency $currency
      * @return JsonResponse
      */
-    public function destroy($id): JsonResponse
+    public function destroy(Currency $currency): JsonResponse
     {
-        //
+        $deleteRecord = $this->currencyRepository->delete($currency);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    private function inputBody(array $details, string $user_id): array {
+        $body = array(
+                    'description'=>$details['description'],
+                    'code'=>$details['code'],
+                    'status'=>$details['status'],
+                    'user_created'=>$user_id,
+                );
+        return $body;
     }
 }
