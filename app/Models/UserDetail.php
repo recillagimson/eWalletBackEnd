30202461b7b50cf53546fc61eacda4c302f9f264
elmer <appearonclouds@gmail.com>
2021-04-12 11:13:04 +0800

    remove softdeletes in model

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use App\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserDetail extends Model
 {
-    use UsesUuid, HasFactory, SoftDeletes;
+    use UsesUuid, HasFactory;
 
     /**
      * The table associated with the model.
