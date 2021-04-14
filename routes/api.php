3fe4c991a65c09b99443d004bcf2d500b687e685
elmer <appearonclouds@gmail.com>
2021-04-14 08:30:21 +0800

    api show user profile for in progress

--- routes/api.php
+++ routes/api.php
@@ -99,6 +99,7 @@ Route::middleware('auth:sanctum')->group(function () {
         ]);
 
         Route::prefix('/user')->group(function (){
+            Route::get('/profile/{user_detail}', [UserProfileController::class, 'show']);
             Route::post('/profile', [UserProfileController::class, 'update']);
         });
     });
