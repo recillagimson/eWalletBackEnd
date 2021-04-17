1be4dfc505c1dc15257e474ddd9ec69b4f927186
DayBet <davette_ct@yahoo.com>
2021-04-17 19:27:12 +0800

    SVC-52 #comment Patch service fee and tier validation

--- app/Repositories/ServiceFee/IServiceFeeRepository.php
+++ app/Repositories/ServiceFee/IServiceFeeRepository.php
@@ -7,7 +7,7 @@ use App\Repositories\IRepository;
 
 interface IServiceFeeRepository extends IRepository
 {
-    public function getByTierAndTransCategoryID(int $tier, string $transCategoryID);
+    public function getByTierAndTransCategoryID(string $tier_id, string $transCategoryID);
     public function list($params = []);
     public function getAmountByTransactionAndUserAccountId(string $transactionCategoryId, string $userAccountId);
 }
