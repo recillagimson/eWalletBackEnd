b30ef1f0072453e5aa573bf77210305f17c30174
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-04-19 17:35:18 +0800

    work in progress waiting for auth user

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -32,7 +32,7 @@ class NotificationController extends Controller
     public function GetAll(): JsonResponse {
         $records = $this->iPushNotificationService->getByUserId(request()->user()->id);
         // dd(request()->user());
-        dd(\Auth::user());
+        // dd(\Auth::user());
         $encryptedResponse = $this->encryptionService->encrypt($records->toArray());
         return response()->json($encryptedResponse, Response::HTTP_OK);
     }
