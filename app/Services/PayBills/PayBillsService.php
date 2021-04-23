15d01906983e7ad3dd86085905dbeaea9ae486d2
Echo <echodunchie@gmail.com>
2021-04-23 16:35:38 +0800

    added controller and services

---
+++ app/Services/PayBills/PayBillsService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\PayBills;
+
+//use App\Repositories\OutSendMoney\IOutSendMoneyRepository;
+
+class PayBillsService implements IPayBillsService
+{
+
+   // private IOutSendMoneyRepository $outSendMoney;
+
+
+    // public function __construct(IOutSendMoneyRepository $outSendMoney,) 
+    // {
+    //     $this->outSendMoney = $outSendMoney;
+    // }    
+
+
+
+
+}
