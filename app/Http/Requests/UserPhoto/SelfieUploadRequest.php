973f2c3082a9e724dbd02bdf2d38b8151309fd78
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-04-26 18:31:12 +0800

    update on selfie upload endpoint

---
+++ app/Http/Requests/UserPhoto/SelfieUploadRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\UserPhoto;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SelfieUploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'selfie_photo' => 'required|max:1024|mimes:jpeg,png',
+        ];
+    }
+}
