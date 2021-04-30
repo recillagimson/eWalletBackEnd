424d6544e1e5593dd939336b3416b5f34dc7098c
Julius Dallego <julius.dallego@hotmail.com>
2021-04-30 12:20:06 +0800

    -

--- app/Services/Utilities/PrepaidLoad/ATM/AtmService.php
+++ app/Services/Utilities/PrepaidLoad/ATM/AtmService.php
@@ -28,7 +28,7 @@ class AtmService implements IAtmService
     public function generateSignature(array $data): string
     {
         $privateKeyContent = Storage::disk('local')->get('/key/partnerid.private.pfx');
-        openssl_pkcs12_read($privateKeyContent, $certs, 'squidpay');
+        openssl_pkcs12_read($privateKeyContent, $certs, '1234567890');
 
         $jsonData = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRESERVE_ZERO_FRACTION);
         openssl_sign($jsonData, $signature, $certs['pkey'], OPENSSL_ALGO_SHA1);
@@ -38,7 +38,7 @@ class AtmService implements IAtmService
     public function verifySignature(array $data, string $base64Signature)
     {
         $signature = base64_decode($base64Signature);
-        $cert = Storage::disk('local')->get('/key/partnerid.public.pem');
+        $cert = Storage::disk('local')->get('/key/partnerid.public.cer');
         $publicKeyId = openssl_get_publickey($cert);
 
         $jsonData = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRESERVE_ZERO_FRACTION);
