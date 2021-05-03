129b1589b4e4ce80a3aff5956a1a89107842da30
Ran Pascua <rpascua@exceture.com>
2021-05-03 12:31:53 +0000

    Update 2021_05_03_200304_set_fields_to_nullable.php

--- database/migrations/2021_05_03_200304_set_fields_to_nullable.php
+++ database/migrations/2021_05_03_200304_set_fields_to_nullable.php
@@ -20,11 +20,11 @@ class SetFieldsToNullable extends Migration
             $table->string('postal_code', 50)->nullable()->change();
             $table->string('place_of_birth', 50)->nullable()->change();
             $table->string('mother_maidenname', 50)->nullable()->change();
-            // $table->string('marital_status_id', 50)->nullable()->change();
+            $table->string('marital_status_id', 50)->nullable()->change();
             $table->string('postal_code', 50)->nullable()->change();
-            // $table->string('nature_of_work_id', 50)->nullable()->change();
+            $table->string('nature_of_work_id', 50)->nullable()->change();
             $table->string('contact_no', 50)->nullable();
-            // $table->string('source_of_fund_id', 50)->nullable()->change();
+            $table->string('source_of_fund_id', 50)->nullable()->change();
             $table->string('occupation', 50)->nullable()->change();
             $table->string('employer', 50)->nullable();
         });
