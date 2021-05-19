bf2d079ec75fc7ddafa0494b43b3107ff90d62f5
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-05-19 16:42:04 +0800

    Role permission and seeder

--- app/Models/Admin/RolePermission.php
+++ app/Models/Admin/RolePermission.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\Admin;
 
+use App\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RolePermission extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, UsesUuid;
 }
