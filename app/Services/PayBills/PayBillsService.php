5682b45b6e434568a45ba3252dcc97b28c0283f7
Echo <echodunchie@gmail.com>
2021-05-25 08:32:24 +0800

    error catch exception for validate and create endpoint

--- app/Services/PayBills/PayBillsService.php
+++ app/Services/PayBills/PayBillsService.php
@@ -14,7 +14,7 @@ use App\Traits\Errors\WithPayBillsErrors;
 use App\Traits\Errors\WithTpaErrors;
 use App\Traits\Transactions\PayBillsHelpers;
 use Illuminate\Http\JsonResponse;
-use Response;
+use Illuminate\Http\Response;
 
 class PayBillsService implements IPayBillsService
 {
@@ -39,7 +39,6 @@ class PayBillsService implements IPayBillsService
     public function getBillers(): array
     {
         $response = $this->bayadCenterService->getBillers();
-
         return (array)json_decode($response->body());
     }
 
@@ -47,7 +46,6 @@ class PayBillsService implements IPayBillsService
     public function getBillerInformation(string $billerCode): array
     {
         $response = $this->bayadCenterService->getBillerInformation($billerCode);
-
         return (array)json_decode($response->body());
     }
 
@@ -55,7 +53,6 @@ class PayBillsService implements IPayBillsService
     public function getWalletBalance(): array
     {
         $response = $this->bayadCenterService->getWalletBalance();
-
         return (array)json_decode($response->body());
     }
 
@@ -63,14 +60,23 @@ class PayBillsService implements IPayBillsService
     public function validateAccount(string $billerCode, string $accountNumber, $data, UserAccount $user): array
     {
         $response = $this->bayadCenterService->validateAccount($billerCode, $accountNumber, $data);
+        $arrayResponse =  (array)json_decode($response);
+
+        if (isset($arrayResponse['exception'])) return $arrayResponse; 
+
         $this->validateTransaction($billerCode, $data, $user);
         return $this->validationResponse($response, $billerCode, $data);
+    
     }
 
 
     public function createPayment(string $billerCode, array $data, UserAccount $user): array
     {
         $response = $this->bayadCenterService->createPayment($billerCode, $data, $user);
+        $arrayResponse =  (array)json_decode($response);
+        
+        if (isset($arrayResponse['exception'])) return $arrayResponse; 
+
         $outPayBills = $this->saveTransaction($user, $billerCode, $response);
         return (array)json_decode($outPayBills);
     }
