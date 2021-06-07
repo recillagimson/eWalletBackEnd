02e7dab69a2e0f26d5f6388d614679a33e27b2c5
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-06-07 20:09:13 +0800

    add validation route for upgrade to silver

--- app/Http/Controllers/UserUtilities/UserProfileController.php
+++ app/Http/Controllers/UserUtilities/UserProfileController.php
@@ -99,6 +99,18 @@ class UserProfileController extends Controller
         return $this->responseService->successResponse($addOrUpdate, SuccessMessages::success);
     }
 
+    public function updateSilverValidation(UpdateProfileSilverRequest $request): JsonResponse {
+        // IF REQUESTING FOR TIER UPDATE
+        if(request()->user() && request()->user()->tier && request()->user()->tier->id !== AccountTiers::tier2) {
+            // VALIDATE IF HAS EXISTING REQUEST
+            $findExistingRequest = $this->userApprovalRepository->getPendingApprovalRequest();
+            if($findExistingRequest) {
+                return $this->tierUpgradeAlreadyExist();
+            }
+        }
+        return $this->responseService->successResponse(null, SuccessMessages::success);
+    }
+
     /**
      * Display the specified resource.
      *
