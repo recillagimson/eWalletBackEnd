160d9d64baed0a998d1460ac0bbc6f3343d4fc5f
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-06-08 19:46:12 +0800

    enable tier approval verification of user

--- app/Http/Controllers/Tier/TierApprovalController.php
+++ app/Http/Controllers/Tier/TierApprovalController.php
@@ -8,6 +8,7 @@ use App\Enums\SuccessMessages;
 use App\Http\Controllers\Controller;
 use App\Repositories\Tier\ITierRepository;
 use App\Services\Tier\ITierApprovalService;
+use App\Http\Requests\Tier\TierUpgradeRequest;
 use App\Http\Requests\Tier\TierApprovalRequest;
 use App\Repositories\Tier\ITierApprovalRepository;
 use App\Services\Utilities\Responses\IResponseService;
