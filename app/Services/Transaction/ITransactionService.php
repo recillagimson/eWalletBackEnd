8e88baac78657ed1ff27f74fd48f667b2395b0fb
Julius Dallego <julius.dallego@hotmail.com>
2021-06-29 03:43:15 +0800

    - process pending transactions on login

--- app/Services/Transaction/ITransactionService.php
+++ app/Services/Transaction/ITransactionService.php
@@ -8,13 +8,15 @@ use App\Models\UserAccount;
 
 interface ITransactionService
 {
-    // public function addAvailableBalance(string $user_account_id, string $current_id, float $available_balance, float $pending_balance);
-    // public function subtractAvailableBalance(string $user_account_id, string $current_id, float $available_balance, float $pending_balance);
-    // public function addPendingBalance(string $user_account_id, string $current_id, float $available_balance, float $pending_balance);
-    // public function subtractPendingBalance(string $user_account_id, string $current_id, float $available_balance, float $pending_balance);
+    public function processUserPending(UserAccount $user);
+
     public function addUserBalanceInfo(string $userAccountId, string $currencyId, float $availableBalance, float $pendingBalance);
+
     public function createUserTransactionEntry(string $userAccountId, string $transactionId, string $referenceNumber, string $transactionCategoryId);
+
     public function createUserTransactionEntryUnauthenticated(string $userAccountId, string $transactionId, string $referenceNumber, float $total_amount, string $transactionCategoryId);
+
     public function generateTransactionHistory(string $userAccountId, string $dateFrom, string $dateTo);
+
     public function downloadCountTotalAmountEachUserCSV(object $request);
 }
