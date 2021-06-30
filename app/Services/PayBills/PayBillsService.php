2c6387716f956f9110a7c57bae7eb80897440606
whee13 <pascua028@yahoo.com>
2021-06-30 22:00:42 +0800

    paybills

--- app/Services/PayBills/PayBillsService.php
+++ app/Services/PayBills/PayBillsService.php
@@ -45,7 +45,7 @@ class PayBillsService implements IPayBillsService
     private IUserTransactionHistoryRepository $transactionHistories;
     private INotificationService $notificationService;
     private ICSVService $csvService;
-    private ILogHistoryRepository $logHistory;
+    //private ILogHistoryRepository $logHistory;
 
     public function __construct(IOutPayBillsRepository $outPayBills, IBayadCenterService $bayadCenterService, IUserDetailRepository $userDetailRepository, IReferenceNumberService $referenceNumberService, IUserBalanceInfoRepository $userBalanceInfo, IServiceFeeRepository $serviceFeeRepository, ITransactionValidationService $transactionValidationService, IUserAccountRepository $userAccountRepository, IOutPayBillsRepository $outPayBillsRepository, IUserTransactionHistoryRepository $transactionHistories, INotificationService $notificationService,
                                 ICSVService $csvService){
