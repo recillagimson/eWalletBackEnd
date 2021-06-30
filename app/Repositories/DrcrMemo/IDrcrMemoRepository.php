f67d7df75110f2c357b5c93fbb3833bb81cbe094
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-06-30 11:43:09 +0800

    add pagination

--- app/Repositories/DrcrMemo/IDrcrMemoRepository.php
+++ app/Repositories/DrcrMemo/IDrcrMemoRepository.php
@@ -7,8 +7,8 @@ use App\Repositories\IRepository;
 interface IDrcrMemoRepository extends IRepository
 {
     public function getByUserAccountID(UserAccount $user);
-    public function getList(UserAccount $user);
-    public function getListByCreatedBy(UserAccount $user, $data);
+    public function getList(UserAccount $user, $per_page = 15);
+    public function getListByCreatedBy(UserAccount $user, $data, $per_page = 15);
     public function getPendingByCreatedBy(UserAccount $user);
     public function getByReferenceNumber(string $referenceNumber);
     public function updateDrcr(UserAccount $user, $data);
