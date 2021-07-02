546b91d4bdc4ee9b2999b6155ed6d605a6bf7260
Echo <echodunchie@gmail.com>
2021-07-02 14:04:52 +0800

    added user transaction history in approval endpoint

--- app/Repositories/DrcrMemo/DrcrMemoRepository.php
+++ app/Repositories/DrcrMemo/DrcrMemoRepository.php
@@ -60,7 +60,7 @@ class DrcrMemoRepository extends Repository implements IDrcrMemoRepository
     public function updateDrcr(UserAccount $user, $data)
     {
         if($data['status'] == DrcrStatus::Approve){
-            return $this->model->where('reference_number', $data['referenceNumber'])->update([
+            $this->model->where('reference_number', $data['referenceNumber'])->update([
                 'status' => DrcrStatus::A,
                 'remarks' => 'Approved Dr/Cr Memo',
                 'approved_by' => $user->id,
@@ -69,7 +69,7 @@ class DrcrMemoRepository extends Repository implements IDrcrMemoRepository
             ]);
         }
         if ($data['status'] == DrcrStatus::Decline) {
-            return $this->model->where('reference_number', $data['referenceNumber'])->update([
+            $this->model->where('reference_number', $data['referenceNumber'])->update([
                 'status' => DrcrStatus::D,
                 'remarks' => $data['remarks'],
                 'declined_by' => $user->id,
@@ -77,6 +77,7 @@ class DrcrMemoRepository extends Repository implements IDrcrMemoRepository
                 'user_updated' => $user->id
             ]);
         }
+        return $this->getByReferenceNumber($data['referenceNumber'])->toArray();
     }
 
     public function updateMemo(UserAccount $user, $data)
@@ -85,7 +86,7 @@ class DrcrMemoRepository extends Repository implements IDrcrMemoRepository
         if($status === 'P') $letterStatus = DrcrStatus::P;
         if($status === 'D') $letterStatus = DrcrStatus::D;
         if($status === 'A') $letterStatus = DrcrStatus::A;
-        return $this->model->where('reference_number', $data['referenceNumber'])->update([
+        $this->model->where('reference_number', $data['referenceNumber'])->update([
             'status' => $letterStatus,
             'type_of_memo' => $data['typeOfMemo'],
             'amount' => $data['amount'],
@@ -93,6 +94,7 @@ class DrcrMemoRepository extends Repository implements IDrcrMemoRepository
             'description' => $data['description'],
             'user_updated' => $user->id
         ]);
+        return $this->getByReferenceNumber($data['referenceNumber'])->toArray();
     }
 
     public function getDRCRMemo()
