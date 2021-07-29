6585797bcc863b9d36b9d1dc6206de5da36d9403
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-07-29 19:44:26 +0800

    revise transaction history, based data on views

--- app/Repositories/UserTransactionHistory/IUserTransactionHistoryRepository.php
+++ app/Repositories/UserTransactionHistory/IUserTransactionHistoryRepository.php
@@ -23,4 +23,8 @@ interface IUserTransactionHistoryRepository extends IRepository
     public function countTransactionHistoryByDateRangeWithAmountLimit(string $from, string $to);
 
     public function isExisting(string $id);
+
+    public function getByAuthUserViaViews();
+
+    public function findTransactionWithRelationViaView(string $id);
 }
