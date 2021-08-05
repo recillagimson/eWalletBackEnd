96c3d848b67e869a03063e60c4daad50951be5ba
Ran Pascua <rpascua@exceture.com>
2021-08-05 06:47:06 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,34 +16,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    
-    $records = BillerReport::where('transaction_date', '>=', '2021-01-01')
-        ->where('transaction_date', '<=', '2021-06-30')->get();
-
-    // return view('reports.out_pay_bills_history.out_pay_bills_history_report', [
-    //     'records' => $records
-    // ]);
-
-    // $records = DRCRProcedure::where('transaction_date', '>=', '2021-01-01')
-    //         ->where('transaction_date', '<=', '2021-06-30')->get();
-
-    $data = [
-        [
-          "2021-05-26 12:09:21",
-          "1000000000002",
-          "Mary Allisson  Lindayag",
-          "0",
-          "Debit",
-          "SM0000323",
-          "10000.000000",
-          null,
-          "0",
-          "SUCCESS",
-        ]
-    ];
-
-    return view('reports.log_histories.log_histories', [
-        'records' => $data
-    ]);
-});
