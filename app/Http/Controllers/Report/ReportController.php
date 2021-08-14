20a4b8f48620ebb181eff039f6b32c5cff4d471f
whee13 <pascua028@yahoo.com>
2021-08-14 18:13:31 +0800

    update response

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -48,6 +48,6 @@ class ReportController extends Controller
     public function print(PrintRequest $request): JsonResponse
     {
         $this->printService->print($request->validated());
-        return $this->responseService->successResponse();
+        return $this->responseService->successResponse($records->toArray(), SuccessMessages::success);
     }
 }
