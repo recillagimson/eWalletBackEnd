a32ca85ef98ee505d21895300af63635ac82479b
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-08-25 21:48:03 +0800

    DBP-75

--- app/Services/BPIService/BPIService.php
+++ app/Services/BPIService/BPIService.php
@@ -96,7 +96,7 @@ class BPIService implements IBPIService
 
         $token = $this->getHeaders($token);
         $response = $this->apiService->get($this->transactionalUrl, $token)->json();
-        \Log::info($response);
+        dd($token);
         dd($response);
         if($response && isset($response['token'])) {
             $jwt = $this->bpiDecryptionJWE($response['token']);
