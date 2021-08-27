980df3c767d0ea85c8bf0889694ef3f9ef6788e9
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-08-27 16:49:33 +0800

    remove line breaks

--- app/Services/BPIService/BPIService.php
+++ app/Services/BPIService/BPIService.php
@@ -96,8 +96,7 @@ class BPIService implements IBPIService
 
         $token = $this->getHeaders($token);
         $response = $this->apiService->get($this->transactionalUrl, $token)->json();
-        dd($response);
-        dd($token);
+
         if($response && isset($response['token'])) {
             $jwt = $this->bpiDecryptionJWE($response['token']);
             \Log::info($jwt);
