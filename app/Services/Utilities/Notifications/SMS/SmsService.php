6eaa14b89948678a2e88f7c460cdb0312dc3dbbb
Wilson Santa Cruz <wilsonsacdalansantacruz@gmail.com>
2021-09-01 15:35:40 +0800

    fix: change notification content on tier upgrade approval

--- app/Services/Utilities/Notifications/SMS/SmsService.php
+++ app/Services/Utilities/Notifications/SMS/SmsService.php
@@ -198,7 +198,7 @@ class SmsService implements ISmsService
 
     public function tierUpgradeNotification(string $to, UserDetail $userDetail, Tier $tier)
     {
-        $content = "Hi Squidee! Your account is now fully verified. Login to your account and enjoy additional features.";
+        $content = "Hi " . $userDetail->first_name . ", Your account is now fully verified. Login to your account to enjoy additional features. Thank you.";
         $this->sendMessages($to, $content);
     }
 
