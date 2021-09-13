b570eaf7df2ec1dfad81323a8afacc05b9cf76a3
Julius Dallego <julius.dallego@hotmail.com>
2021-09-13 22:35:11 +0800

    - Added logging of product list response in atm service

--- app/Services/Utilities/PrepaidLoad/ATM/AtmService.php
+++ app/Services/Utilities/PrepaidLoad/ATM/AtmService.php
@@ -117,16 +117,18 @@ class AtmService implements IAtmService
 
         $url = $this->baseUrl . $this->productsUrl;
         $response = $this->apiService->post($url, $data, $headers);
-        Log::info('ATM Product Lists', $response->json());
 
         if ($response->successful()) {
             $data = $response->json();
             $state = $data['responseCode'];
             if ($state === AtmPrepaidResponseCodes::requestReceived) {
                 $prefixes = collect($data['data']);
-                return ($provider == TopupTypes::atm_epin) ?
+                $products = ($provider == TopupTypes::atm_epin) ?
                     $prefixes->where('productType', $provider)->sortBy(['provider', 'productCode', 'denominations']) :
                     $prefixes->where('provider', $provider)->sortBy(['provider', 'productCode', 'denominations']);
+
+                Log::info($provider, $products);
+                return $products;
             }
         }
 
