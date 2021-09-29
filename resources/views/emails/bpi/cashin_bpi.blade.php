6afb2ac5331ff0619b34e8586fded8d31ae95eeb
whee13 <pascua028@yahoo.com>
2021-09-29 22:03:31 +0800

    update notification

--- resources/views/emails/bpi/cashin_bpi.blade.php
+++ resources/views/emails/bpi/cashin_bpi.blade.php
@@ -1,8 +1,7 @@
-Hi {{$firstName}} ! 
+
+<h2>Hi {{ $firstName }}!,</h2>
 <br/>
 <br/>
-You have successfully added funds to your wallet via BPI 
-<br/>
-on {{ $createdAt }} Service fee for this transaction is PHP 0.00. 
-<br/>
-Your new balance is {{ $newBalance }} with reference no. {{ $referenceNumber }};
+    You have successfully added funds to your wallet via BPI on {{ $createdAt }} Service fee for this transaction is PHP 0.00.  Your new balance is {{ $newBalance }} with reference no. {{ $referenceNumber }}. Thank you for using SquidPay! 
+
+
