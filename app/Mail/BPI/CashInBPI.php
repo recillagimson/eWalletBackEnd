20b3fcaf358427383d6e39672e65c94e3901fafa
Ran Pascua <rpascua@exceture.com>
2021-09-29 13:56:53 +0000

    Update CashInBPI.php

--- app/Mail/BPI/CashInBPI.php
+++ app/Mail/BPI/CashInBPI.php
@@ -40,7 +40,7 @@ class CashInBPI extends Mailable
             ->subject('SquidPay - Cash In BPI')
             ->with([
                 'firstName' => $this->userDetail->first_name,
-                'refNo' => $this->referenceNumber,
+                'referenceNumber' => $this->referenceNumber,
                 'newBalance' => number_format($this->newBalance, 2),
                 'createdAt' => Carbon::now()->setTimezone('Asia/Manila')->format('D, M d, Y h:m A')
             ]);
