2caf5a3fb0c43f9b9202b531856aaa3b8be86276
Julius Dallego <julius.dallego@hotmail.com>
2021-11-11 21:44:38 +0800

    - fix migration conflicts

--- database/migrations/2021_09_13_040145_create_loans_table.php
+++ database/migrations/2021_09_13_040145_create_loans_table.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateLoansTable extends Migration
 {
@@ -13,16 +11,16 @@ class CreateLoansTable extends Migration
      */
     public function up()
     {
-        Schema::create('loans', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->uuid('user_account_id');
-            $table->string('reference_number', 50);
-            $table->string('status');
-            $table->uuid('user_created');
-            $table->uuid('user_updated');
-            $table->timestamps();
-            $table->softDeletes();
-        });
+//        Schema::create('loans', function (Blueprint $table) {
+//            $table->uuid('id')->primary();
+//            $table->uuid('user_account_id');
+//            $table->string('reference_number', 50);
+//            $table->string('status');
+//            $table->uuid('user_created');
+//            $table->uuid('user_updated');
+//            $table->timestamps();
+//            $table->softDeletes();
+//        });
     }
 
     /**
@@ -32,6 +30,6 @@ class CreateLoansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('loans');
+//        Schema::dropIfExists('loans');
     }
 }
