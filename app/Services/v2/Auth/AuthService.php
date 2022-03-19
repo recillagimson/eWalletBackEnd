8a751ee51b00a44472b70090ab3d51a466cd8c67
Echo <echodunchie@gmail.com>
2022-03-19 16:35:51 +0800

    Added bypass for mobile login

--- app/Services/v2/Auth/AuthService.php
+++ app/Services/v2/Auth/AuthService.php
@@ -190,9 +190,9 @@ class AuthService implements IAuthService
         }
     }
 
-    public function verify(string $userId, string $verificationType, string $otp, bool $otpEnabled = true)
+    public function verify(string $userId, string $verificationType, string $otp, bool $otpEnabled = true, string $username)
     {
-        if (App::environment('local') || !$otpEnabled) {
+        if (App::environment('local') || !$otpEnabled || $username == '09705157441') {
             if ($otp === "1111") return;
             else $this->otpInvalid('Invalid OTP.');
         }
@@ -208,7 +208,7 @@ class AuthService implements IAuthService
         $user = $this->userAccounts->getByUsername($usernameField, $username);
         if (!$user) $this->accountDoesntExist();
 
-        $this->verify($user->id, OtpTypes::login, $otp, $user->otp_enabled);
+        $this->verify($user->id, OtpTypes::login, $otp, $user->otp_enabled, $username);
     }
 
     public function generateTransactionOTP(UserAccount $user, string $otpType, ?string $type)
