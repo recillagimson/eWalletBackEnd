e68ddc0dc34f657af8d1491c72b981b7b98f4b9f
Echo <echodunchie@gmail.com>
2022-05-12 14:13:31 +0800

    Converted to string in cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,6 @@ class Cors
             ->header('Access-Control-Allow-Origin', 'https://admin-my.squid.ph')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
-            ->header('Access-Control-Allow-Credentials', true);
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
