67734e30db2cb4699984534ba29e090e87ef1cb2
Echo <echodunchie@gmail.com>
2022-05-12 14:09:06 +0800

    added Acces Control alow in Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'https://admin-my.squid.ph')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'); 
+            ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
+            ->header('Access-Control-Allow-Credentials', '*');
     }
 }
