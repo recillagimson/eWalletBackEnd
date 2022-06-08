78b5aa60edae468f4ca8b42024fd09fbfa87e659
Echo <echodunchie@gmail.com>
2022-06-08 14:58:57 +0800

    added registrationservice disable

--- app/Services/v2/Auth/Registration/RegistrationService.php
+++ app/Services/v2/Auth/Registration/RegistrationService.php
@@ -48,98 +48,98 @@ class RegistrationService implements IRegistrationService
         $this->qrTransactions = $qrTransactions;
     }
 
-    public function validateAccount(string $usernameField, string $username)
-    {
-        $user = $this->userAccounts->getByUsername($usernameField, $username);
-        if (!$user) return;
+    // public function validateAccount(string $usernameField, string $username)
+    // {
+    //     $user = $this->userAccounts->getByUsername($usernameField, $username);
+    //     if (!$user) return;
 
-        if ($user->verified) $this->accountAlreadyTaken();
-        $user->forceDelete();
-    }
+    //     if ($user->verified) $this->accountAlreadyTaken();
+    //     $user->forceDelete();
+    // }
 
-    public function register(array $newUser, string $usernameField)
-    {
-        $this->validateAccount($usernameField, $newUser[$usernameField]);
+    // public function register(array $newUser, string $usernameField)
+    // {
+    //     $this->validateAccount($usernameField, $newUser[$usernameField]);
 
-        $newUser['password'] = Hash::make($newUser['password']);
-        $newUser['tier_id'] = AccountTiers::tier1;
-        $newUser['account_number'] = $this->userAccountNumbers->generateNo();
+    //     $newUser['password'] = Hash::make($newUser['password']);
+    //     $newUser['tier_id'] = AccountTiers::tier1;
+    //     $newUser['account_number'] = $this->userAccountNumbers->generateNo();
 
-        $user = $this->userAccounts->create($newUser);
+    //     $user = $this->userAccounts->create($newUser);
 
-        //LOG INITIAL CHANGES TO USER KEYS (PASSWORD / PIN CODES)
-        $this->passwordHistories->log($user->id, $newUser['password']);
-        // $this->pinCodeHistories->log($user->id, $newUser['pin_code']);
+    //     //LOG INITIAL CHANGES TO USER KEYS (PASSWORD / PIN CODES)
+    //     $this->passwordHistories->log($user->id, $newUser['password']);
+    //     // $this->pinCodeHistories->log($user->id, $newUser['pin_code']);
 
-        $this->authService->sendOTP($usernameField, $newUser[$usernameField], OtpTypes::registration);
-        return $user;
-    }
+    //     $this->authService->sendOTP($usernameField, $newUser[$usernameField], OtpTypes::registration);
+    //     return $user;
+    // }
 
-    public function verifyAccount(string $usernameField, string $username, string $otp)
-    {
-        $user = $this->userAccounts->getByUsername($usernameField, $username);
-        if (!$user) $this->accountDoesntExist();
+    // public function verifyAccount(string $usernameField, string $username, string $otp)
+    // {
+    //     $user = $this->userAccounts->getByUsername($usernameField, $username);
+    //     if (!$user) $this->accountDoesntExist();
 
-        if ($user->verified) $this->accountAlreadyVerified();
+    //     if ($user->verified) $this->accountAlreadyVerified();
 
-        $this->authService->verify($user->id, OtpTypes::registration, $otp, $user->otp_enabled);
+    //     $this->authService->verify($user->id, OtpTypes::registration, $otp, $user->otp_enabled);
 
-        return $user;
-    }
+    //     return $user;
+    // }
 
-    public function registerPin(array $data, string $usernameField)
-    {
-        $user = $this->userAccounts->getByUsername($usernameField, $data[$usernameField]);
-        if (!$user) $this->accountDoesntExist();
+    // public function registerPin(array $data, string $usernameField)
+    // {
+    //     $user = $this->userAccounts->getByUsername($usernameField, $data[$usernameField]);
+    //     if (!$user) $this->accountDoesntExist();
 
-        if ($user->verified) $this->accountAlreadyVerified();
+    //     if ($user->verified) $this->accountAlreadyVerified();
 
-        $user->pin_code = Hash::make($data['pin_code']);
-        $user->verified = true;
-        $user->save();
+    //     $user->pin_code = Hash::make($data['pin_code']);
+    //     $user->verified = true;
+    //     $user->save();
 
-        $this->pinCodeHistories->log($user->id, $data['pin_code']);
+    //     $this->pinCodeHistories->log($user->id, $data['pin_code']);
 
-        $this->setupAccount($user->id);
+    //     $this->setupAccount($user->id);
 
-        $token = $this->authService->mobileLogin($usernameField, $data);
+    //     $token = $this->authService->mobileLogin($usernameField, $data);
 
-        return $token;
-    }
+    //     return $token;
+    // }
 
-    /*
-    |--------------------------------------------------------------------------
-    | PRIVATE METHODS
-    |--------------------------------------------------------------------------
-    */
+    // /*
+    // |--------------------------------------------------------------------------
+    // | PRIVATE METHODS
+    // |--------------------------------------------------------------------------
+    // */
 
-    private function setupAccount(string $userId)
-    {
-        $this->setupUserBalance($userId);
-        $this->setupQrTransaction($userId);
-    }
+    // private function setupAccount(string $userId)
+    // {
+    //     $this->setupUserBalance($userId);
+    //     $this->setupQrTransaction($userId);
+    // }
 
-    private function setupUserBalance(string $userId)
-    {
-        $balance = [
-            'user_account_id' => $userId,
-            'currency_id' => Currencies::philippinePeso,
-            'user_created' => $userId,
-            'user_updated' => $userId,
-        ];
+    // private function setupUserBalance(string $userId)
+    // {
+    //     $balance = [
+    //         'user_account_id' => $userId,
+    //         'currency_id' => Currencies::philippinePeso,
+    //         'user_created' => $userId,
+    //         'user_updated' => $userId,
+    //     ];
 
-        $this->userBalances->create($balance);
-    }
+    //     $this->userBalances->create($balance);
+    // }
 
-    private function setupQrTransaction(string $userId)
-    {
-        $qrTransactions = [
-            'user_account_id' => $userId,
-            'amount' => 0,
-            'status' => 1,
-            'user_created' => $userId
-        ];
+    // private function setupQrTransaction(string $userId)
+    // {
+    //     $qrTransactions = [
+    //         'user_account_id' => $userId,
+    //         'amount' => 0,
+    //         'status' => 1,
+    //         'user_created' => $userId
+    //     ];
 
-        $this->qrTransactions->create($qrTransactions);
-    }
+    //     $this->qrTransactions->create($qrTransactions);
+    // }
 }
