56f371b6f5c46bc2ead76e515914b3d748e9d750
Echo <echodunchie@gmail.com>
2022-06-08 15:12:13 +0800

    disable bpi cash in

--- app/Http/Controllers/BPIController.php
+++ app/Http/Controllers/BPIController.php
@@ -30,64 +30,64 @@ class BPIController extends Controller
         $this->transactionValidationService = $transactionValidationService;
     }
 
-    public function bpiAuth(BPIAuthRequest $request): JsonResponse
-    {
-        $response = $this->bpiService->bpiAuth($request->code);
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function bpiAuth(BPIAuthRequest $request): JsonResponse
+    // {
+    //     $response = $this->bpiService->bpiAuth($request->code);
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function getAccounts(BPIGetAccountRequest $request): JsonResponse
-    {
-        $response = $this->bpiService->getAccounts($request->token);
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function getAccounts(BPIGetAccountRequest $request): JsonResponse
+    // {
+    //     $response = $this->bpiService->getAccounts($request->token);
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function fundTopUp(BPIFundTopUpRequest $request): JsonResponse
-    {
-        $data = [
-            'accountNumberToken' => $request->accountNumberToken,
-            'amount' => $request->amount,
-            'remarks' => $request->remarks
-        ];
-        $response = $this->bpiService->fundTopUp($data, $request->token);
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function fundTopUp(BPIFundTopUpRequest $request): JsonResponse
+    // {
+    //     $data = [
+    //         'accountNumberToken' => $request->accountNumberToken,
+    //         'amount' => $request->amount,
+    //         'remarks' => $request->remarks
+    //     ];
+    //     $response = $this->bpiService->fundTopUp($data, $request->token);
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function otp(BPIOTPRequest $request): JsonResponse
-    {
-        $response = $this->bpiService->otp($request->all());
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function otp(BPIOTPRequest $request): JsonResponse
+    // {
+    //     $response = $this->bpiService->otp($request->all());
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function process(BPIProcessRequest $request): JsonResponse
-    {
-        $response = $this->bpiService->process($request->all(), request()->user()->id);
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function process(BPIProcessRequest $request): JsonResponse
+    // {
+    //     $response = $this->bpiService->process($request->all(), request()->user()->id);
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function status(BPIStatusRequest $request): JsonResponse
-    {
-        $response = $this->bpiService->status($request->all());
-        return $this->responseService->successResponse($response, SuccessMessages::success);
-    }
+    // public function status(BPIStatusRequest $request): JsonResponse
+    // {
+    //     $response = $this->bpiService->status($request->all());
+    //     return $this->responseService->successResponse($response, SuccessMessages::success);
+    // }
 
-    public function getBPIAuthUrl(BPITransactionLimitValidationRequest $request) {
-        $this->transactionValidationService->checkUserMonthlyTransactionLimit(request()->user(), $request->amount, TransactionCategoryIds::cashinBPI);
-        return $this->responseService->successResponse(
-            [
-                'login_url' => config('bpi.loginUrl')
-            ],
-            SuccessMessages::success
-        );
-    }
+    // public function getBPIAuthUrl(BPITransactionLimitValidationRequest $request) {
+    //     $this->transactionValidationService->checkUserMonthlyTransactionLimit(request()->user(), $request->amount, TransactionCategoryIds::cashinBPI);
+    //     return $this->responseService->successResponse(
+    //         [
+    //             'login_url' => config('bpi.loginUrl')
+    //         ],
+    //         SuccessMessages::success
+    //     );
+    // }
 
-    public function getCredentials() {
-        return $this->responseService->successResponse(
-            [
-                'clientId' => config('bpi.clientId'),
-                'clientSecret' => config('bpi.clientSecret')
-            ],
-            SuccessMessages::success
-        );
-    }
+    // public function getCredentials() {
+    //     return $this->responseService->successResponse(
+    //         [
+    //             'clientId' => config('bpi.clientId'),
+    //             'clientSecret' => config('bpi.clientSecret')
+    //         ],
+    //         SuccessMessages::success
+    //     );
+    // }
 }
